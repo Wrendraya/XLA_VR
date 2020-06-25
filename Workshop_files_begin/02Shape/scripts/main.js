7658d25427193ad438c08ea94cb58848199379a6
Céline Besse <45100489+Wrendraya@users.noreply.github.com>
2020-06-25 12:06:26 +0200

    Tinkering Movements

---
+++ Workshop_files_begin/02Shape/scripts/main.js
@@ -0,0 +1,15 @@
+//changing individual properties with code and using setInterval
+var rotationSpeed = 0.04;
+var myOtherBox = document.getElementById('planetWater');
+
+function spin(){
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.y);
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.z);
+	
+ }
+
+setInterval(spin, 16); //equivalent to 60 fps
